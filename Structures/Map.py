239e1c1e6e4f88b970e31cf6b961f7c25e789500
Keaton Lee <keatonlee@Keatons-MacBook-Pro.local>
2023-02-11 21:07:08 -0500

    fix Map.py

--- Structures/Map.py
+++ Structures/Map.py
@@ -12,6 +12,7 @@ class Map:
     # The state of the class, just initialized in 'Algorithm'
     available_points:list[Vector2] = []
     warehouse:Vector2 = Vector2(0,0)
+    parcels:list[tuple[Parcel, float]] = []
     
     
     
@@ -21,8 +22,8 @@ class Map:
     @staticmethod
     def open_street_map_processor(parcel_info: list[tuple[str,]], points: list[Vector2]):
         #This takes in raw data from open street maps and the parcels to be delivered and sorts it into the state of Map. TODO - make it actually take Open Street Map data and parse it (instead of taking in pre-generated points)
-        available_points = points
-        warehouse = Map.name_to_location()
+        Map.available_points = points
+        Map.warehouse = Map.name_to_location()
         
         Map.create_parcels(parcel_info)
     
@@ -33,12 +34,11 @@ class Map:
     
     @staticmethod
     def create_parcels(parcel_info: list[tuple[str,]]):
-        parcels = [] 
         for entry in parcel_info:
             parcel = Parcel(entry[0], Map.name_to_location())
             distance = Map.find_distance(parcel.position,Map.warehouse)
             Map.parcels.append((parcel,distance))
-        Map.parcels = sorted(parcels, key=itemgetter(1))
+        Map.parcels = sorted(Map.parcels, key=itemgetter(1))
 
     @staticmethod
     def find_distance(target: Vector2, source: Vector2):
--- basicplot.py
+++ basicplot.py
@@ -21,7 +21,7 @@ size = width, height = 800, 800
 #Map Creation
 parcel_info = create_parcel_info()
 #TODO - This next line should take random map data
-city_map = Map.open_street_map_processor(parcel_info, random_points(len(parcel_info) + 10, width, height))
+Map.open_street_map_processor(parcel_info, random_points(len(parcel_info) + 10, width, height))
 
 # creating surfaces to draw on, background and dots layer
 # (layer for truck is stored in truck class)
@@ -29,14 +29,14 @@ screen = pygame.display.set_mode(size)
 dots = pygame.Surface.copy(screen)
 
 coords_list = []
-for entry in city_map.parcels:
+for entry in Map.parcels:
     parcel = entry[0]
     coords_list.append(parcel.position)
 
-route = sort_by_distance(coords_list, city_map.warehouse)
+route = sort_by_distance(coords_list, Map.warehouse)
 
 tkun = Truck()
-tkun.drive_route(route, city_map.warehouse)
+tkun.drive_route(route, Map.warehouse)
 
 while True:
     for event in pygame.event.get():
@@ -45,7 +45,7 @@ while True:
     for p in coords_list:
         pygame.draw.circle(dots, "white", p, 3.0)
 
-    pygame.draw.circle(dots, "green", city_map.warehouse, 4.0)
+    pygame.draw.circle(dots, "green", Map.warehouse, 4.0)
 
     screen.fill("black")
     screen.blit(tkun.surf, tkun.surf.get_rect())
