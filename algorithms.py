db805abd17801c2d99573731ed4797c49c31ec2c
Keaton Lee <keatonlee@Keatons-MacBook-Pro.local>
2023-02-11 19:02:42 -0500

    add algorithms.py

---
+++ algorithms.py
@@ -0,0 +1,41 @@
+import pygame
+import random
+import sys
+import math
+import time
+
+from pygame import Vector2
+
+from Structures.Map import Map
+from config import *
+
+def construct_quadtree():
+    quadtree = [[{} for i in range(granularity)] for i in range(granularity)]
+
+    leftmost_x = map_center_x - map_radius # TODO Make sure coordinates match, is top left (0,0)?
+    topmost_y = map_center_y - map_radius
+
+    # TODO replace buildings with parcels
+    for building in Map.buildings:
+        box_x = math.floor((building.position.x - leftmost_x) / box_width)
+        box_y = math.floor((building.position.y - topmost_y) / box_height)
+        
+        # for case when point on right or bottom edge
+        try:
+            quadtree[box_x][box_y].add(building)
+        except IndexError:
+            if box_x == map_center_x + map_radius: # TODO Replace map_radius
+                box_x -= 1
+            if box_y == map_center_y + map_radius:
+                box_y -= 1
+            quadtree[box_x][box_y].add(building)
+
+    return quadtree
+
+def assign_destinations():
+    for index, building in reversed(list(enumerate(Map.buildings))):
+        if building.assigned:
+            del Map.buildings[index]
+        else:
+            # continue
+            pass
--- algorythms.py
+++ algorythms.py
@@ -6,8 +6,6 @@ import time
 
 from pygame import Vector2
 
-from Structures.Map import Map
-
 def spiral_deliver(points: list[Vector2], origin: Vector2) -> list[Vector2]:
     sortedpoints = sort_by_distance(points)
     far = sortedpoints[-1]
@@ -38,38 +36,3 @@ def sort_by_distance(points: list[Vector2], origin: Vector2) -> list[Vector2]:
     print (distances)
 
     return sortedpoints
-
-def construct_quadtree():
-    leftmost_x = rightmost_x = Map.buildings[0].position.x
-    topmost_y = bottommost_y = Map.buildings[0].position.y
-
-    for building in Map.buildings:
-        if building.position.x < leftmost_x:
-            leftmost_x = building.position.x
-        elif building.position.x > rightmost_x:
-            rightmost_x = building.position.x
-        elif building.position.y < topmost_y:
-            topmost_y = building.position.y
-        elif building.position.y > bottommost_y:
-            bottommost_y = building.position.y
-
-    box_width = (rightmost_x - leftmost_x) / 10     # use constant from config.py
-    box_height = (bottommost_y - topmost_y) / 10    # use constant from config.py
-
-    quadtree = [[[] for i in range(10)] for i in range(10)]     # use constant from config.py
-
-    # will error for rightmost_x and bottommost_y
-    for building in Map.buildings:
-        x = math.floor((building.position.x - leftmost_x) / box_width)
-        y = math.floor((building.position.y - topmost_y) / box_height)
-        quadtree[x][y].append(building)
-
-    return leftmost_x, topmost_y, box_width, box_height, quadtree
-
-def assign_destinations():
-    for building in Map.buildings[::-1]:
-        if building.assigned:
-            # delete building from Map.buildings (maybe iterate by index instead of by value)
-            pass
-        else:
-            pass
