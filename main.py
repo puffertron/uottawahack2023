98040d35a15e1f272a14960cff3789e6b0520143
Keaton Lee <keatonlee@Keatons-MacBook-Pro.local>
2023-02-12 01:40:20 -0500

    completed basic version of assign_destinations()

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 #imports
-import basicplot
+# import basicplot
 from algorithms import *
+
+from algorythms import * # TEMPORARY
 
 #constants
 RANDOM_DELIVERIES = True
 PRESET_DELIVERIES = ["Put house names here"]
 
 #main
-parcels = create_parcels()
+# parcels = create_parcels()
+
+# TEMPORARY
+size = width, height = 800, 800
+parcel_info = create_parcel_info()  
+Map.open_street_map_processor(parcel_info, random_points(len(parcel_info) + 10, width, height))
+# TEMPORARY
+
+quadtree = construct_quadtree()
+
+assign_destinations(quadtree)
