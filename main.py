89cdd5157b96769a2300d21b259788bcd73b51c1
Kidane Gabay <puffertron@gmail.com>
2023-02-12 09:56:47 -0500

    changed main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 from algorithms import *
 import config
 from Structures.Map import Map
+from Structures.StreetMap import StreetMap
 
 from algorythms import * # TEMPORARY
 
@@ -15,6 +16,11 @@ OPEN_STREET_MAPS_DATA = random_points(len(PRESET_DELIVERIES) + 10, config.map_wi
 initialize_objects(OPEN_STREET_MAPS_DATA, PRESET_DELIVERIES)
 quadtree = construct_quadtree()
 clusters = assign_clusters(Map.parcels,max((config.map_width,config.map_height)),quadtree)
+
+stmap = StreetMap()
+s,x = stmap.initialise_road_segments()
+roads = stmap.merge_segments(s, x)
+stmap.populate_linked_list_network(roads)
 
 for parcel in Map.parcels:
     print(parcel.cluster_id)
